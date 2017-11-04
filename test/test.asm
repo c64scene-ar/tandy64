be56c7b9245c57f5194c6ca12ad9dd036e4ecd5b
Ricardo Quesada <ricardoquesada@gmail.com>
2017-11-03 18:52:30 -0700

    much faster plasma code

--- test/test.asm
+++ test/test.asm
@@ -12,16 +12,67 @@ extern ZTimerOn, ZTimerOff, ZTimerReport
 ;=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-;
 section .text
 ..start:
-        sub     ax,ax
-        sub     al,al
-        sub     ah,ah
-        sub     cx,cx
-        sub     cl,cl
-        sub     ch,ch
-        sub     di,di
-        sub     si,si
-        sub     bp,bp
+        mov     ax,stack
+        cli                                     ;disable interrupts while
+        mov     ss,ax                           ; setting the stack pointer
+        mov     sp,stacktop
+        sti
+
+        cld
+
+        mov     ax,data
+        mov     ds,ax
+
+        call    ZTimerOn
+        %assign XX 0
+        %rep    2048
+                mov     al,[buffer+XX]          ;fetch plasma X buffer
+                add     al,[buffer+XX]          ; and add it to plasma Y buffer
+        %assign XX XX+1
+        %endrep
+        call    ZTimerOff
+        call    ZTimerReport
+
+        mov     si,buffer
+        mov     di,buffer
+        call    ZTimerOn
+        %assign XX 0
+        %rep    2048
+                mov     al,[si]
+                add     al,[di]                 ; and add it to plasma Y buffer
+                inc     si
+                inc     di
+        %assign XX XX+1
+        %endrep
+        call    ZTimerOff
+        call    ZTimerReport
+
+        mov     si,buffer
+        mov     di,buffer
+        call    ZTimerOn
+        %assign XX 0
+        %rep    2048
+                lodsb
+                add     al,[di]                 ; and add it to plasma Y buffer
+                inc     di
+        %assign XX XX+1
+        %endrep
+        call    ZTimerOff
+        call    ZTimerReport
 
         mov     ax,4c00h
         int     21h                             ;exit to DOS
 
+
+;=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-;
+section .data data
+
+buffer:
+        resb    4096
+
+;=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-;
+; STACK
+;=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-;
+section .stack stack
+        resb 2048
+stacktop:
