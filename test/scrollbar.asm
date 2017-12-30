8456edbf996cb4a72d68032a5027635dac6ea113
Ricardo Quesada <ricardoquesada@gmail.com>
2017-12-30 09:23:19 -0800

    scrollbar test: 3 different ones

--- test/scrollbar.asm
+++ test/scrollbar.asm
@@ -125,7 +125,7 @@ PIT_DIVIDER equ (262*76)                        ;262 lines * 76 PIT cycles each
         mov     es,ax                           ;es = page 0
 
         ;PIC
-        mov     ax,new_i08_test
+        mov     ax,new_i08
         mov     dx,cs
         xchg    ax,[es:8*4]                     ;new/old IRQ 8: offset
         xchg    dx,[es:8*4+2]                   ;new/old IRQ 8: segment
@@ -203,58 +203,66 @@ new_i08:
         mov     bx,0x001f                       ;bl = color to update (white=0x1f)
                                                 ;bh = 0. needed later
 
+        ;
+        ; "normal" rasterbar with noise
+        ;
         %rep 16
-                WAIT_HORIZONTAL_RETRACE                 ;reset to register again
+                WAIT_HORIZONTAL_RETRACE         ;reset to register again
+
+                mov     al,bl                   ;color to update
+                out     dx,al                   ;dx=0x03da (register)
+
+                lodsb                           ;load one color value in al
+                out     dx,al                   ;update color (data)
+
+                mov     al,bh                   ;set reg 0 so display works again
+                out     dx,al                   ;(register)
+
+        %endrep
+
+        times   200 nop                         ;leave some blank lines
+        mov     si,colors
+        ;
+        ; rasterbar without noise
+        ;
+        %rep 16
+                WAIT_HORIZONTAL_RETRACE         ;reset to register again
 
                 ;335 works: big fat raster
                 times  40 nop
 
-                mov     al,bl                           ;color to update
-                out     dx,al                           ;dx=0x03da (register)
+                mov     al,bl                   ;color to update
+                out     dx,al                   ;dx=0x03da (register)
 
-                lodsb                                   ;load one color value in al
-                out     dx,al                           ;update color (data)
+                lodsb                           ;load one color value in al
+                out     dx,al                   ;update color (data)
 
-                mov     al,bh                           ;set reg 0 so display works again
-                out     dx,al                           ;(register)
-
+                mov     al,bh                   ;set reg 0 so display works again
+                out     dx,al                   ;(register)
         %endrep
 
-        WAIT_HORIZONTAL_RETRACE                 ;reset to register again
-
-        inc     byte [tick]
-
-        mov     al,0x20
-        out     0x20,al
-        iret
-
-;=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-;
-new_i08_test:
+        times   200 nop                         ;leave some blank lines
         mov     si,colors
-        mov     dx,VGA_ADDRESS
-        mov     bx,0x001f                       ;bl = color to update (white=0x1f)
-                                                ;bh = 0. needed later
-
+        ;
+        ; rasterbar without noise (using nops instead of horiz retrace)
+        ;
         WAIT_HORIZONTAL_RETRACE                 ;reset to register again
-
-        ;335 works: big fat raster
-        times  40 nop
+        times  40 nop                           ;sync
         %rep 16
-                mov     al,bl                           ;color to update
-                out     dx,al                           ;dx=0x03da (register)
+                mov     al,bl                   ;color to update
+                out     dx,al                   ;dx=0x03da (register)
 
-                lodsb                                   ;load one color value in al
-                out     dx,al                           ;update color (data)
+                lodsb                           ;load one color value in al
+                out     dx,al                   ;update color (data)
 
-                mov     al,bh                           ;set reg 0 so display works again
-                out     dx,al                           ;(register)
+                mov     al,bh                   ;set reg 0 so display works again
+                out     dx,al                   ;(register)
 
-                in      al,dx                           ;reset
+                in      al,dx
 
-                times  120 nop
+                times   80 nop                  ;sync
         %endrep
 
-        WAIT_HORIZONTAL_RETRACE                 ;reset to register again
 
         inc     byte [tick]
 
