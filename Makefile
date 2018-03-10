bd54bae88df7d1c5eae673cbd0b7b184b764b066
Ricardo Quesada <ricardoquesada@gmail.com>
2018-03-10 12:06:12 -0800

    PCJr: add checks for PCjr and memory

--- Makefile
+++ Makefile
@@ -10,7 +10,7 @@ LDFLAGS = -oEXE -m
 default: $(TARGET)
 all: res default
 
-OBJECTS = main.o intro.o pre_intro.o post_intro.o
+OBJECTS = main.o intro.o pre_intro.o post_intro.o detect_card.o pztimer.o
 
 %.o: src/%.asm
 	$(ASM) $(ASMFLAGS) $< -o $@
--- src/main.asm
+++ src/main.asm
@@ -7,12 +7,15 @@ bits    16
 cpu     8086
 
 extern intro_start, pre_intro_start, post_intro_start
+extern detect_card
 
 ;=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-;
 ; CODE
 ;=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-;
 section .text
 ..start:
+        mov     ax,data
+        mov     ds,ax
         mov     ax,stack
         cli                                     ;disable interrupts while
         mov     ss,ax                           ; setting the stack pointer
@@ -21,6 +24,18 @@ section .text
 
         cld
 
+        call    detect_card
+        cmp     al,2
+        jz      is_pcjr
+        jmp     not_pcjr
+
+is_pcjr:
+        mov     ax,cs
+        cmp     ax,0x1000
+        jae     above_128k
+        jmp     not_128k
+
+above_128k:
         call    pre_intro_start
 
 ;        mov     ax,0x0009
@@ -31,6 +46,21 @@ section .text
 
         jmp     cleanup
 
+;=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-;
+not_pcjr:
+        mov     ah,9
+        mov     dx,label_not_pcjr
+        int     0x21
+        mov     ax,4c00h
+        int     21h                             ;exit to DOS
+
+;=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-;
+not_128k:
+        mov     ah,9
+        mov     dx,label_not_128k
+        int     0x21
+        mov     ax,4c00h
+        int     21h                             ;exit to DOS
 
 ;=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-;
 cleanup:
@@ -45,6 +75,17 @@ cleanup:
 ;        push    ax
 ;        push    bx
 ;        retf
+
+;=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-;
+; DATA
+;=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-;
+section .data
+
+label_not_pcjr:
+        db 'Tandy64 Jr. only works on IBM PCjr computers.',13,10,'$'
+
+label_not_128k:
+        db 'PCjr with at least 256kb RAM needed. Code should run above 128kb RAM',13,10,'$'
 
 ;=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-;
 ; STACK
